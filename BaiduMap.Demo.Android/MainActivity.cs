882c9149fc7800d32b37f1540a896f43fc61c701
Carl <wenguang@shinetechsoftware.com>
2019-07-22 23:36:43 +0800

    Update MainActivity.cs

--- BaiduMap.Demo.Android/MainActivity.cs
+++ BaiduMap.Demo.Android/MainActivity.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.Graphics;
 using Android.OS;
 using Android.Runtime;
 using Android.Support.Design.Widget;
@@ -12,6 +13,8 @@ using Com.Baidu.Mapapi;
 using Com.Baidu.Mapapi.Map;
 using Com.Baidu.Mapapi.Model;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using static Com.Baidu.Mapapi.Map.BaiduMap;
 
 namespace BaiduMap.Demo.Android
@@ -53,16 +56,73 @@ namespace BaiduMap.Demo.Android
             var marker = new MarkerOptions()
                     .InvokePosition(new LatLng(40.023537, 116.289429))
                     .InvokeIcon(BitmapDescriptorFactory.FromResource(Resource.Mipmap.ic_launcher))
-                    .InvokeTitle("Title")
+                    .Visible(true)
                     .Draggable(true);
-            Map.Map.AddOverlay(marker);
 
-            Map.Map.MarkerClick += (s, e) =>
-            {
-                //e.Handled = true;
-                var m = e.P0;
-                Snackbar.Make(Map, m.Title, Snackbar.LengthShort).Show();
+            var circle = new CircleOptions()
+                .InvokeCenter(new LatLng(43.023637, 116.287429))
+                .InvokeFillColor(Color.Coral.ToArgb())
+                .InvokeStroke(new Stroke(20, Color.Green.ToArgb()))
+                .InvokeRadius(2000)
+                .Visible(true);
+            var points = new List<LatLng> {
+                new LatLng(40.123537, 119.289429),
+                new LatLng(42.124537, 116.289429),
+                new LatLng(40.123537, 116.288429),
+                new LatLng(41.123737, 117.289489)
             };
+            var polygon = new PolygonOptions()
+                .InvokeFillColor(Color.Red.ToArgb())
+                .InvokePoints(points)
+                .Visible(true);
+            var pointsLine = new List<LatLng> {
+                new LatLng(40.223537, 116.289429),
+                new LatLng(40.124537, 116.489429),
+                new LatLng(41.123537, 116.288429),
+                new LatLng(41.123737, 117.289489)
+            };
+            var line = new PolylineOptions()
+                .InvokePoints(pointsLine)
+                .InvokeColor(Color.Blue.ToArgb())
+                .InvokeWidth(20)
+                .KeepScale(true)
+                .Visible(true);
+            Map.Map.AddOverlays(new List<OverlayOptions> { marker, circle, polygon, line });
+            //var q = Map.Map.AddOverlay(marker);
+            //var w = Map.Map.AddOverlay(circle);
+            //var a = Map.Map.AddOverlay(polygon);
+            //var r = Map.Map.AddOverlay(line);
+
+            Map.Map.MarkerClick += (_, e) =>
+            {
+                e.Handled = true;
+                var m = e.P0;
+                ShowSnackbar("Marker " + m.Position.ToString());
+            };
+
+            Map.Map.PolylineClick += (s, e) =>
+            {
+                e.Handled = true;
+                var latlng = e.P0.Points.Last();
+                var random = new Random();
+                var x = random.Next(1);
+                var y = random.NextDouble();
+                if (x % 2 == 0)
+                    latlng.Latitude += y;
+                else
+                    latlng.Longitude += y;
+                e.P0.Points.Add(latlng);
+            };
+
+            Map.Map.MapClick += (s, e) => ShowSnackbar("Map " + e.P0.ToString());
+            Map.Map.MapDoubleClick += (s, e) => ShowSnackbar("MapDouble " + e.P0.ToString());
+            Map.Map.MapLongClick += (s, e) => ShowSnackbar("MapLong " + e.P0.ToString());
+            Map.Map.MapPoiClick += (s, e) => ShowSnackbar("MapPoi " + e.P0.ToString());
+        }
+
+        private void ShowSnackbar(string msg)
+        {
+            Snackbar.Make(Map, msg, Snackbar.LengthShort).Show();
         }
 
         protected override void OnStart()
